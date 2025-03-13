1febeca78f8607d44865cd120202c418e22e00af
Jairo Blanco <JairoBlanco15@gmail.com>
2025-03-13 09:37:51 +0100

    Add Blazor front to Aspire. Wireup ApiClient using Url from Aspire

--- FitTech.API.Client/ServiceCollectionExtensions.cs
+++ FitTech.API.Client/ServiceCollectionExtensions.cs
@@ -1,15 +1,36 @@
+using FitTech.API.Client.Configuration;
 using FitTech.API.Client.Contracts;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FitTech.API.Client;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddFitTechApiClient(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddFitTechApiClient(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
     {
+        var fitTechConfig = configuration.GetSection(FitTechApiConfiguration.ConfigSectionName)
+            .Get<FitTechApiConfiguration>();
+
+        ArgumentNullException.ThrowIfNull(fitTechConfig);
+
+        serviceCollection.AddSingleton(fitTechConfig);
+
         serviceCollection
             .AddHttpClient()
-            .AddTransient<IFitTechApiClient, FitTechApiFitTechApiClient>();
+            .AddTransient<IFitTechApiClient>(c =>
+            {
+                var httpClientFactory = c.GetRequiredService<IHttpClientFactory>();
+
+                var fitTechApiClient =
+                    new FitTechApiFitTechApiClient(httpClientFactory.CreateClient(nameof(FitTechApiFitTechApiClient)))
+                    {
+                        BaseUrl = fitTechConfig.Url
+                    };
+
+                return fitTechApiClient;
+            });
 
         return serviceCollection;
     }
