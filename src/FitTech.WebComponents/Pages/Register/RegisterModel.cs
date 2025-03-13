ae46d5413fdc5a1ace907c99ff34c5343fea4a69
Yeray <yerayblanco@hotmail.com>
2025-03-13 18:00:39 +0100

    Fix ids

--- src/FitTech.WebComponents/Pages/Register/RegisterModel.cs
+++ src/FitTech.WebComponents/Pages/Register/RegisterModel.cs
@@ -9,17 +9,25 @@ namespace FitTech.WebComponents.Pages.Register
 {
    public class RegisterModel
     {
-       
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string ConfirmPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(20, ErrorMessage = "El nombre no puede tener más de 20 caracteres.")]
         public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(20, ErrorMessage = "El apellido no puede tener más de 20 caracteres.")]
         public string LastName { get; set; } = null!;
-        public string PhoneNumber { get; set; } = null!;//Necesario para el registro???
-        public string Address { get; set; } = null!;//Necesario para el registro???
-        public string City { get; set; } = null!; //Necesario para el registro???
-        public string State { get; set; } = null!;//Necesario para el registro???
-        public string ZipCode { get; set; } = null!;//Necesario para el registro???
-        public string Country { get; set; } = null!;//Necesario para el registro???
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Por favor ingresa un correo electrónico válido.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Debes confirmar tu contraseña.")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; } = null!;
     }
 }
