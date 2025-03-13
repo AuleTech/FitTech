e5fe1240550586c53b88b4af13b34ab2f304a60a
Yeray <yerayblanco@hotmail.com>
2025-03-13 09:58:02 +0100

    Added RegisterPage

---
+++ src/FitTech.WebComponents/Pages/Register/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTech.WebComponents.Pages.Register
+{
+   public class RegisterModel
+    {
+
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;//Necesario para el registro???
+        public string Address { get; set; } = null!;//Necesario para el registro???
+        public string City { get; set; } = null!; //Necesario para el registro???
+        public string State { get; set; } = null!;//Necesario para el registro???
+        public string ZipCode { get; set; } = null!;//Necesario para el registro???
+        public string Country { get; set; } = null!;//Necesario para el registro???
+    }
+}
