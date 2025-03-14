9d0661974faea1c79d487bbdf81d3f8559f9ceb1
Yeray <yerayblanco@hotmail.com>
2025-03-14 08:53:55 +0100

    Add RecoveryPasswordPage

---
+++ src/FitTech.WebComponents/Pages/Recovery Password/RecoveryPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTech.WebComponents.Pages.Recovery_Password
+{
+    public class RecoveryPasswordModel
+    {
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Por favor ingresa un correo electrónico válido.")]
+        public string Email { get; set; } = null!;
+    }
+}
