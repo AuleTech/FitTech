e368ba022a2203861fd0b8378368cdf60ee1817e
Jairo Blanco <JairoBlanco15@gmail.com>
2025-04-26 09:25:31 +0200

    - Fix packages - Remove typos and loading pages

--- src/FitTech.AppHost/Program.cs
+++ src/FitTech.AppHost/Program.cs
@@ -2,20 +2,16 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder
-    .AddPostgres("postgres")
-    .WithLifetime(ContainerLifetime.Persistent);
+    .AddPostgres("postgres");
 
 var postgresdb = postgres.AddDatabase("fittechdb");
-
 var fitTechApi = builder.AddProject<FitTech_API>("fittech-api")
     .WithReference(postgresdb)
     .WaitFor(postgresdb)
     .WithExternalHttpEndpoints();
 
-
 _ = builder.AddProject<FitTech_Trainer_Wasm>("trainer-web")
-    .WaitFor(fitTechApi)
-    .WithEnvironment("FitTechApi__url", fitTechApi.GetEndpoint("https"));
+    .WaitFor(fitTechApi); 
 
 
 builder.Build().Run();
