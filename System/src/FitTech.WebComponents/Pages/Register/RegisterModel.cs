023437563a9071078b48ab3e341cb38816b9c537
Yeray <111679635+YerayBA@users.noreply.github.com>
2025-05-20 22:39:06 +0200

    Fix Icons Style and Regular Expression

--- System/src/FitTech.WebComponents/Pages/Register/RegisterModel.cs
+++ System/src/FitTech.WebComponents/Pages/Register/RegisterModel.cs
@@ -12,14 +12,17 @@ namespace FitTech.WebComponents.Pages.Register
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
         [StringLength(20, ErrorMessage = "El nombre no puede tener más de 20 caracteres.")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'-]+$", ErrorMessage = "El nombre solo puede contener letras y guiones.")]
         public string FirstName { get; set; } = null!;
 
         [Required(ErrorMessage = "El apellido es obligatorio.")]
         [StringLength(20, ErrorMessage = "El apellido no puede tener más de 20 caracteres.")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'-]+$", ErrorMessage = "El apellido solo puede contener letras y guiones.")]
         public string LastName { get; set; } = null!;
 
         [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
         [EmailAddress(ErrorMessage = "Por favor ingresa un correo electrónico válido.")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Por favor ingresa un correo electrónico válido y sin caracteres no permitidos.")]
         public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "La contraseña es obligatoria.")]
