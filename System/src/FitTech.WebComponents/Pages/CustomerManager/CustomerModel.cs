0206fc40c4e01dae747bd380db8d70d57df8b34e
Yeray <111679635+YerayBA@users.noreply.github.com>
2025-05-24 22:46:48 +0200

    Remove AddUserForm in SearchBar, Add CustomerManger page, AddNewClientPage , addCustomerModel and AddSubscription model

---
+++ System/src/FitTech.WebComponents/Pages/CustomerManager/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FitTech.WebComponents.Pages.CustomerManager;
+
+public class CustomerModel
+{
+    public string NameUser { get; set; } = null!;
+    
+    public string LastNameuser { get; set; } = null!;
+    
+    public string EmailUser { get; set; } = null!;
+    
+    public DateOnly Birthdate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+    public int PhoneNumber { get; set; } 
+
+}
