a56f02598d17628936edda89208ebc79cf7d61a0
Yeray <111679635+YerayBA@users.noreply.github.com>
2025-05-25 22:43:08 +0200

    Add components

---
+++ System/src/FitTech.WebComponents/Pages/CustomerManager/DetailsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FitTech.WebComponents.Pages.CustomerManager;
+
+public class DetailsModel
+{
+    public int TrainingHours { get; set; }
+    public string TrainingModel { get; set; } = string.Empty;
+    public DateOnly Event { get; set; }
+    public string Center { get; set; } = string.Empty;
+    
+    public int HorasSueños { get; set; }
+    
+}
