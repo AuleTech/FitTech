263c8439534365c1ba4f49f65b12bc619bc985bc
Jairo Blanco <JairoBlanco15@gmail.com>
2025-06-08 21:01:32 +0200

    Use AllAnyOrigin

--- System/src/FitTech.API/Program.cs
+++ System/src/FitTech.API/Program.cs
@@ -29,7 +29,7 @@ builder.Services
     .AddOpenApi()
     .AddCors( c => c.AddPolicy("FitTechCorsPolicy", policyBuilder =>
     {
-        policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7083", "http://localhost:5174");
+        policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
 
     }))
     .AddPersistence(connectionString);
