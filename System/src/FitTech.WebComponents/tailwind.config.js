479786a06d456fa2be05b7306598a8b9d2364668
Yeray <111679635+YerayBA@users.noreply.github.com>
2025-10-02 21:12:47 +0200

    colors fittech

--- System/src/FitTech.WebComponents/tailwind.config.js
+++ System/src/FitTech.WebComponents/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         extend: {
             colors: {
                 'fittech-primary': 'rgba(173,197,57,1)',
-                'fittech-secondary': 'rgba(73,73,71,1)'
+                'fittech-secondary': 'rgba(73,73,71,1)',
+                'fittech-tertiary': 'rgba(229, 235, 234, 1)'
             }
         }
     },
