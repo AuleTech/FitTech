7f9189edb291aed9955222f7696ec54b6b90a012
Jairo Blanco <JairoBlanco15@gmail.com>
2025-10-13 12:42:44 +0200

    Add Unit tests and workflow

---
+++ System/src/FitTech.UnitTests/Data/Generators/FitTechUserTestGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using FitTech.Domain.Aggregates.AuthAggregate;
+
+namespace FitTech.UnitTests.Data.Generators;
+
+public sealed class FitTechUserTestGenerator : Faker<FitTechUser>
+{
+    public FitTechUserTestGenerator()
+    {
+        RuleFor(x => x.Id, faker => Guid.CreateVersion7());
+        RuleFor(x => x.Email, faker => faker.Person.Email);
+    }
+}
