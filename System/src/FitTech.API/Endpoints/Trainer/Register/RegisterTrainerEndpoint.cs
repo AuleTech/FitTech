49d37b2552296060e97bc6f5fed21887192537b9
Jairo Blanco <JairoBlanco15@gmail.com>
2025-10-13 12:15:54 +0200

    Add IntegrationTests

--- System/src/FitTech.API/Endpoints/Trainer/Register/RegisterTrainerEndpoint.cs
+++ System/src/FitTech.API/Endpoints/Trainer/Register/RegisterTrainerEndpoint.cs
@@ -3,29 +3,23 @@ using AuleTech.Core.Patterns.CQRS;
 using AuleTech.Core.Patterns.Result;
 using FastEndpoints;
 using FitTech.Application.Commands.Trainer.Register;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FitTech.API.Endpoints.Trainer.Register;
 
-[HttpPost("/user/add-trainer")]
-public class RegisterTrainerEndPoint : Endpoint<RegisterTrainerRequest>
+[AllowAnonymous]
+[HttpPost("/trainer/register")]
+public class RegisterTrainerEndpoint : Endpoint<RegisterTrainerRequest>
 {
     private readonly IAuleTechCommandHandler<RegisterTrainerCommand, Result> _commandHandler;
 
-    public RegisterTrainerEndPoint(IAuleTechCommandHandler<RegisterTrainerCommand, Result> commandHandler)
+    public RegisterTrainerEndpoint(IAuleTechCommandHandler<RegisterTrainerCommand, Result> commandHandler)
     {
         _commandHandler = commandHandler;
     }
 
     public override async Task HandleAsync(RegisterTrainerRequest req, CancellationToken ct)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-        if (string.IsNullOrWhiteSpace(userId))
-        {
-            await Send.UnauthorizedAsync(ct);
-            return;
-        }
-
         var result = await _commandHandler.HandleAsync(req.ToCommand(), ct);
 
         if (!result.Succeeded)
