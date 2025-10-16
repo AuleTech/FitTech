65af7af09b310e04c9bafe1ed63e0ab78030aea0
Jairo Blanco <JairoBlanco15@gmail.com>
2025-10-16 10:29:48 +0200

    Fix flaky test

--- System/src/FitTech.UnitTests/Queries/Trainer/GetInvitationsQueryTests.cs
+++ System/src/FitTech.UnitTests/Queries/Trainer/GetInvitationsQueryTests.cs
@@ -35,7 +35,7 @@ internal class GetInvitationsQueryTests : BaseCqrsUnitTest<GetInvitationsQuery, 
 
         var randomInvitation = result.Value.First(x => x.Status != nameof(InvitationStatus.Accepted));
         randomInvitation.Status.Should().BeOneOf(Enum.GetNames<InvitationStatus>());
-        randomInvitation.CreatedUtc.Should().NotBeBefore(DateTime.UtcNow.AddDays(-15));
+        randomInvitation.CreatedUtc.Date.Should().NotBeBefore(DateTime.UtcNow.AddDays(-15).Date);
         randomInvitation.ClientEmail.Should().NotBeNullOrWhiteSpace();
     }
 }
