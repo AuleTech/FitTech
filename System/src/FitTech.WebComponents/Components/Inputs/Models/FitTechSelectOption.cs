de2791c27aca846c6f678588a96a87288b92d88e
Jairo Blanco <JairoBlanco15@gmail.com>
2025-12-19 14:41:28 +0100

    Add multi select

---
+++ System/src/FitTech.WebComponents/Components/Inputs/Models/FitTechSelectOption.cs
@@ -0,0 +1,12 @@
+﻿namespace FitTech.WebComponents.Components.Inputs.Models;
+
+public class FitTechSelectOption
+{
+    public FitTechSelectOption(string value, string? displayValue =null)
+    {
+        Value = value;
+        DisplayValue = displayValue;
+    }
+    public string Value { get; set; }
+    public string? DisplayValue { get; set; }
+}
