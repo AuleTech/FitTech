65a98afa45dbf2a0721d9c9ab661602c6dd9e6ed
Jairo Blanco <JairoBlanco15@gmail.com>
2025-12-22 15:09:05 +0100

    Add Api request

---
+++ System/src/FitTech.Client.Mobile/Pages/Auth/Registration/Models/CredentialsForm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitTech.Client.Mobile.Pages.Auth.Registration.Models;
+
+public class CredentialsForm : IValidatableObject
+{
+    [Required(ErrorMessage = "Introduce la contraseña")]
+    public string Password { get; set; } = null!;
+
+    [Required(ErrorMessage = "Introduce la contraseña")]
+    public string RepeatPassword { get; set; } = null!;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Password != RepeatPassword)
+        {
+            yield return new ValidationResult(
+                "Las contraseñas no coinciden",
+                [nameof(RepeatPassword)]
+            );
+        }
+    }
+}
---
+++ System/src/FitTech.Client.Mobile/Pages/Auth/Registration/Models/TrainingPreferencesForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitTech.Client.Mobile.Pages.Auth.Registration.Models;
+
+public class TrainingPreferencesForm
+{
+    [Required(ErrorMessage = "Introduce tu disponibilidad")]
+    public int? AvailableDays { get; set; }
+
+    [Required]
+    [MinLength(1, ErrorMessage = "Selecciona al menos 1 opción")]
+    public string[] FavouriteExercises { get; set; } = [];
+
+    [Required(ErrorMessage = "Selecciona tu objetivo")]
+    public string? Goal { get; set; }
+}
