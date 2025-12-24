ca34709cb5bd31355facef51148e2be435091a42
Jairo Blanco <JairoBlanco15@gmail.com>
2025-12-24 15:35:57 +0100

    add snackbar

---
+++ System/src/FitTech.WebComponents/Components/Snackbar/FitTechSnackbarDisplay.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FitTech.WebComponents.Components.Snackbar;
+
+public partial class FitTechSnackbarDisplay
+{
+    [Inject] public IFitTechSnackbarService SnackbarService { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        SnackbarService.OnSnackbarChanged += OnSnackbarChange;
+    }
+
+    private void OnSnackbarChange()
+    {
+        InvokeAsync(StateHasChanged);
+    }
+}
